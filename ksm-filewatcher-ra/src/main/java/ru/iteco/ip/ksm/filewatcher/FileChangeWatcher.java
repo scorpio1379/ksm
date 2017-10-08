81b26c7251f4921126ee23f0e036090791f71c45
Scorpio <scorpio.thedark@gmail.com>
2017-10-08 22:49:52 +0300

    OGM v5 Branch

--- ksm-filewatcher-ra/src/main/java/ru/iteco/ip/ksm/filewatcher/FileChangeWatcher.java
+++
@@ -1,9 +0,0 @@
-package ru.iteco.ip.ksm.filewatcher;
-
-/**
- * Created by Scorpio on 06.10.2017.
- * утащено отсюда http://robertpanzer.github.io/blog/2014/inboundra-nointfmdbs.html
- * https://github.com/robertpanzer/filesystemwatch-connector
- */
-public interface FileChangeWatcher {
-}
